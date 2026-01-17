d5e05894706d1afe707c778655b03e094a7f1a6b
Felipe Mattos <166661571+felipervm@users.noreply.github.com>
2026-01-17 14:15:52 -0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,23 @@
+# Sample Coding Questions 01 Week 01
+# Felipe da Rocha Vieira Mattos
+
+# Defining Variables
+numbers = [1, 4, 7, 9]
+
+# Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Fully bracketed version of the given expression
+e = (a - ((b ** c) // d)) + (a % c)
+
+# Formatting
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# Common Functions
+userAge = int(input("Enter your age: "))
+userAge += 22
+print("Now showing the shop items filtered by age:", userAge)
